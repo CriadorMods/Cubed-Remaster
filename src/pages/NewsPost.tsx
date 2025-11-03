dd5a4f52aa28a249cf2a9dd2d1ead8ea0489f3a5
Criador_Mods <158328258+CriadorMods@users.noreply.github.com>
2025-11-03 17:58:05 +0000

    More 1:1 NewsPost

--- src/pages/NewsPost.tsx
+++ src/pages/NewsPost.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 // Utils
 import { createSlug } from "@/utils/string";
+
+// Icons
+import { FaFacebookF, FaTwitter, FaVk } from "react-icons/fa";
+import { ImReddit } from "react-icons/im";
 
 interface INewsPost {
   _id: string;
@@ -20,8 +24,10 @@ const NewsPost: React.FC = () => {
   const { header } = useParams();
   const [newsPost, setNewsPost] = useState<INewsPost | null>(null);
   const navigate = useNavigate();
+  const idkRef = useRef<HTMLDivElement | null>(null);
+  const [isFixed, setIsFixed] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const news = async () => {
       try {
         const response = await fetch(
@@ -50,18 +56,51 @@ const NewsPost: React.FC = () => {
     news();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!idkRef.current) return;
+      const rect = idkRef.current.getBoundingClientRect();
+      const shouldBeFixed = rect.bottom <= 0;
+      
+      setIsFixed(prev => (prev !== shouldBeFixed ? shouldBeFixed : prev));
+    };
+    
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
+
   return (
     <div className="mt-[3.2rem] flex flex-col w-screen min-h-[calc(100vh-51.2px)] justify-start items-center">
-      <div className="w-full">
+      <div className="relative w-full">
 
         <div
+        ref={idkRef}
         style={{ backgroundImage: `url('${newsPost?.image}')`, }}
-        className="w-full h-[62vh] bg-center bg-no-repeat bg-fixed bg-cover -translate-y-[1.75em]"
+        className="w-full h-[80vh] bg-center bg-no-repeat bg-fixed bg-cover -translate-y-[1.75em]"
         />
 
       </div>
 
-            <div className="w-full max-w-[80%] md:max-w-[40%] my-12 leading-[1.428571] font-OpenSans text-black text-md">
+            <div className="relative w-full max-w-[55%] md:max-w-[65%] my-6 leading-normal font-OpenSans text-black text-md">
+
+                <div className={`${isFixed ? "fixed" : "absolute"} top-[11.5em] ml-[-6em] flex flex-col gap-1.75`}>
+                <i className="bg-[#236293] text-white w-[2em] h-[2em] flex justify-center items-center rounded-full cursor-pointer hover:brightness-130 transition-all ease duration-300">
+                  <FaFacebookF/>
+                </i>
+
+                <i className="bg-[#38ace0] text-white w-[2em] h-[2em] flex justify-center items-center rounded-full cursor-pointer hover:brightness-130 transition-all ease duration-300">
+                  <FaTwitter/>
+                </i>
+
+                <i className="bg-[#456890] text-white w-[2em] h-[2em] flex justify-center items-center rounded-full cursor-pointer hover:brightness-130 transition-all ease duration-300">
+                  <FaVk/>
+                </i>
+
+                <i className="bg-[#ff8558] text-white w-[2em] h-[2em] flex justify-center items-center rounded-full cursor-pointer hover:brightness-130 transition-all ease duration-300">
+                  <ImReddit/>
+                </i>
+              </div>
+
               <ReactMarkdown
                 remarkPlugins={[remarkGfm]}
                 components={{
@@ -105,8 +144,8 @@ const NewsPost: React.FC = () => {
                     />
                   ),
                   hr: ({ node, ...props }) => (
-                    <hr
-                      className="my-5 h-1 w-full bg-[#b3b3b3]"
+                    <div
+                      className="my-5 h-0.75 w-full bg-[#b3b3b3]"
                       {...props}
                     />
                   ),
